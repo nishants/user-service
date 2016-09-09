6b95ac014778088f38a518802f75799711e4f53c
nishant <nishant.singh87@gmail.com>
2016-09-10 03:29:11 +0530

    config for dev and prod.

--- app/config.js
+++ app/config.js
@@ -1,41 +1,46 @@
 
-var prod = {
-  db: {
-    user: "vierbvtiwvgelt",
-    password: "GCorFWB6MigCYrDAH3oXKEaXNF",
-    host: "ec2-54-243-204-57.compute-1.amazonaws.com:5432",
-    database: "d3g1dr5aueqm7f",
-    dialect: "postgres",
-    ssl: true,
-    url: function () {
-      var url = "<dialect>://<user>:<password>@<host>/<database>";
-      return url
-          .replace("<dialect>"  , this.dialect)
-          .replace("<user>"     , this.user)
-          .replace("<password>" , this.password)
-          .replace("<host>"     , this.host)
-          .replace("<database>", this.database);
+var prod = function(){
+  return {
+    db: {
+      user: "vierbvtiwvgelt",
+      password: "GCorFWB6MigCYrDAH3oXKEaXNF",
+      host: "ec2-54-243-204-57.compute-1.amazonaws.com:5432",
+      database: "d3g1dr5aueqm7f",
+      dialect: "postgres",
+      ssl: true,
+      url: function () {
+        var url = "<dialect>://<user>:<password>@<host>/<database>";
+        return url
+            .replace("<dialect>"  , this.dialect)
+            .replace("<user>"     , this.user)
+            .replace("<password>" , this.password)
+            .replace("<host>"     , this.host)
+            .replace("<database>", this.database);
+      }
     }
-  }
+  };
 };
 
-var dev = {
-  db: {
-    user:     "dawn",
-    password: "12345",
-    host: "localhost",
-    database: "d3g1dr5aueqm7f",
-    dialect: "postgres",
-    ssl: true,
-    url: function () {
-      var url = "<dialect>://<user>:<password>@<host>/<database>";
-      return url
-          .replace("<dialect>"  , this.dialect)
-          .replace("<user>"     , this.user)
-          .replace("<password>" , this.password)
-          .replace("<host>"     , this.host)
-          .replace("<database>", this.database);
+var dev = function(){
+  return {
+    db: {
+      user: "dawn",
+          password: "12345",
+          host: "localhost",
+          database: "postgres",
+          dialect: "postgres",
+          ssl: false,
+          url: function () {
+        var url = "<dialect>://<user>:<password>@<host>/<database>";
+        return url
+            .replace("<dialect>"  , this.dialect)
+            .replace("<user>"     , this.user)
+            .replace("<password>" , this.password)
+            .replace("<host>"     , this.host)
+            .replace("<database>", this.database);
+      }
     }
-  }
+  };
 };
-module.exports = prod;
+
+module.exports = process.env.ENV === "production" ?  prod() : dev() ;
